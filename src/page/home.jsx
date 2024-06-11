90a3f1f6e49bb4aec8b976344a9bad0b30bc1e5f
Natnatee <natnatee_mond@hotmail.com>
2024-06-11 18:48:02 +0700

    v

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -1,9 +1,11 @@
 import Slide from "../components/components_home/slide";
+import WhyMe from "../components/components_home/why_me";
 
 function Home() {
   return (
     <div>
       <Slide />
+      <WhyMe/> 
     </div>
   );
 }
