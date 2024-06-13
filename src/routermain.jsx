3094e8adedba8c68890efee0f0b9e41209a0e675
Natnatee <natnatee_mond@hotmail.com>
2024-06-13 14:54:08 +0700

    v

--- src/routermain.jsx
+++ src/routermain.jsx
@@ -6,6 +6,7 @@ import Home from "./page/home";
 import Sell from "./page/sell";
 import Buy from "./page/buy";
 import { Card } from "./components/champ/card";
+import ProductCard_Info from "./components/components_home/productcard_info";
 
 const RouterMain = () => {
   // const [products, setProducts] = useState([]);
@@ -46,6 +47,16 @@ const RouterMain = () => {
         <>
           <Navbar />
           <Card />
+          <Footer />
+        </>
+      ),
+    },
+    {
+      path: "/productcard_info",
+      element: (
+        <>
+          <Navbar />
+          <ProductCard_Info/>
           <Footer />
         </>
       ),
