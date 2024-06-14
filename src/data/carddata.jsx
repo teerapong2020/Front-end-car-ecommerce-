5a834385ff62c3e2a151ec9776c7ca225b530521
teeerapong2020 <teerapong.chue@gmail.com>
2024-06-14 09:35:53 +0700

    l

--- src/data/carddata.jsx
+++ src/data/carddata.jsx
@@ -4,6 +4,7 @@ import p911 from '../assets/cardcar.jsx/porche/911/porche_911_main.jpg'
 
 
 export const Carddata = () => {
+  
     const pdcards = [
       {
         name: 'BMW X5',
@@ -13,16 +14,80 @@ export const Carddata = () => {
         image: p911,
         location: 'Pattaya, Thailand',
         price: '฿25,000',
-        rentLink: '/view_rent/bmw/X5.html',
-        detailLink: '/view_rent/bmw/X5.html'
+        now: '/view_rent/bmw/X5.html',
+        detail: '/view_rent/bmw/X5.html'
       },
+      {
+        name: 'BMW X5',
+        rating: '4.9/5.0',
+        ratingIcon: '',
+        category: 'SUV',
+        image: p911,
+        location: 'Pattaya, Thailand',
+        price: '฿25,000',
+        now: '/view_rent/bmw/X5.html',
+        detail: '/view_rent/bmw/X5.html'
+      },
+      {
+        name: 'BMW X5',
+        rating: '4.9/5.0',
+        ratingIcon: '',
+        category: 'SUV',
+        image: p911,
+        location: 'Pattaya, Thailand',
+        price: '฿25,000',
+        now: '/view_rent/bmw/X5.html',
+        detail: '/view_rent/bmw/X5.html'
+      },
+      {
+        name: 'BMW X5',
+        rating: '4.9/5.0',
+        ratingIcon: '',
+        category: 'SUV',
+        image: p911,
+        location: 'Pattaya, Thailand',
+        price: '฿25,000',
+        now: '/view_rent/bmw/X5.html',
+        detail: '/view_rent/bmw/X5.html'
+      },
+      {
+        name: 'BMW X5',
+        rating: '4.9/5.0',
+        ratingIcon: '',
+        category: 'SUV',
+        image: p911,
+        location: 'Pattaya, Thailand',
+        price: '฿25,000',
+        now: '/view_rent/bmw/X5.html',
+        detail: '/view_rent/bmw/X5.html'
+      },
+      {
+        name: 'BMW X5',
+        rating: '4.9/5.0',
+        ratingIcon: '',
+        category: 'SUV',
+        image: p911,
+        location: 'Pattaya, Thailand',
+        price: '฿25,000',
+        now: '/view_rent/bmw/X5.html',
+        detail: '/view_rent/bmw/X5.html'
+      },
+      
      
       ];
   return (
+    <>
     <div className=" flex flex-wrap justify-center gap-8 my-20 md:my-20">
     {pdcards.map((pdcard, index) => (
       <Card key={index} pdcard={pdcard} />
     ))}
   </div>
+  <div className='flex justify-center'>
+  <button>◀️</button>
+    <button>1</button>
+    <button>▶️</button>
+  </div>
+  
+  </>
   )
 }
