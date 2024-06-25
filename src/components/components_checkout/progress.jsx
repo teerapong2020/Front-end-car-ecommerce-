3ab71a325794b44806e12190faff7619988ab20d
Tranfersafety <ratthaphumi.safe@gmail.com>
2024-06-25 21:15:42 +0700

    fix all css to figma

--- src/components/components_checkout/progress.jsx
+++ src/components/components_checkout/progress.jsx
@@ -4,43 +4,43 @@ function Progress() {
   return (
     <article
       id="statusContainer"
-      className="flex flex-row max-w-[1145px] mt-14 mb-12 relative"
+      className="flex flex-row max-w-[1145px] mt-[55px] mb-12 relative"
     >
       <div className="flex flex-col items-center">
         <div
           id="innerCircle"
-          className="w-16 h-16 rounded-full bg-[#1E3769] relative"
+          className="w-[55px] h-[55px] rounded-full bg-[#1E3769] relative"
         >
           <div
             id="leftCheck"
-            className="w-[4.5px] h-[16px] top-[29px] left-[22px] -rotate-45 rounded-full bg-white absolute"
+            className="w-[4px] h-[16px] top-[26px] left-[18px] -rotate-45 rounded-full bg-white absolute"
           ></div>
           <div
             id="rightCheck"
-            className="w-[4.5px] h-[31px] top-[16px] left-[36px] rotate-45 rounded-full bg-white absolute"
+            className="w-[4px] h-[31px] top-[13px] left-[32px] rotate-45 rounded-full bg-white absolute"
           ></div>
         </div>
         <p className="text-[14px]">กรอกข้อมูล</p>
       </div>
-      <div className="w-[198px] h-1 bg-[#CDECFF] relative top-8"></div>
+      <div className="w-[203px] h-1 bg-[#CDECFF] relative top-[25px]"></div>
       <div className="flex flex-col items-center ">
         <div
           id="circleStatus"
-          className="w-16 h-16 bg-[#CDECFF] rounded-full grid place-content-center"
+          className="w-[45px] h-[45px]  bg-[#CDECFF] rounded-full grid place-content-center mt-1"
         >
           2
         </div>
         <p className="text-[14px]">ชำระเงิน</p>
       </div>
-      <div className="w-[198px] h-1 bg-[#CDECFF] relative top-8"></div>
+      <div className="w-[203px] h-1 bg-[#CDECFF] relative top-[25px]"></div>
       <div className="flex flex-col items-center relative ">
         <div
           id="circleStatus"
-          className="w-16 h-16 bg-[#CDECFF] rounded-full grid place-content-center "
+          className="w-[45px]  h-[45px]  bg-[#CDECFF] rounded-full grid place-content-center mt-1 ml-[2px]"
         >
           3
         </div>
-        <p className="text-[14px] top-[64px] left-[1px] w-20  absolute ">
+        <p className="text-[14px] top-[49px] left-[1px] w-20  absolute ">
           เช่ารถสำเร็จ
         </p>
       </div>
