0247e31f060eb43a5087090e66368edc4cf3bb1c
teeerapong2020 <teerapong.chue@gmail.com>
2024-07-12 13:53:51 +0700

    sfsdf

--- src/data/selldata.jsx
+++ src/data/selldata.jsx
@@ -1,19 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import { NewCard } from "../components/components_home/newcard";
-import example_products from "./example_products";
-import { onSearchCar } from "../components/API/API_Cars";
-import Search from "../components/champ/search"; 
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { NewCard } from '../components/components_home/newcard';
+import example_products from './example_products';
+import { onSearchCar } from '../components/API/API_Cars';
+import Search from '../components/champ/search';
 
 const imagesPerPage = 6;
 
 export const Selldata = () => {
   const location = useLocation();
-  const initialSearchValue = location.state?.searchValue || "";
+  const initialSearchValue = location.state?.searchValue || '';
   const [currentPage, setCurrentPage] = useState(1);
   const [products, setProducts] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
-
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -22,7 +21,7 @@ export const Selldata = () => {
         setProducts(products);
         setSearchResults(products);
       } catch (error) {
-        console.error("Error fetching products:", error);
+        console.error('Error fetching products:', error);
       }
     };
     fetchProducts();
@@ -40,14 +39,14 @@ export const Selldata = () => {
       setSearchResults(result);
       setCurrentPage(1);
     } catch (error) {
-      console.error("Error searching for cars:", error);
+      console.error('Error searching for cars:', error);
     }
   };
 
-  const handleClearSearch = async()=>{
-    const showall = await example_products()
-    setSearchResults(showall)
-  }
+  const handleClearSearch = async () => {
+    const showall = await example_products();
+    setSearchResults(showall);
+  };
 
   const indexOfLastImage = currentPage * imagesPerPage;
   const indexOfFirstImage = indexOfLastImage - imagesPerPage;
@@ -56,12 +55,12 @@ export const Selldata = () => {
     setCurrentPage(pageNumber);
     window.scrollTo({
       top: 0,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   };
 
   const pageNumbers = [];
-  for (let i = 1; i <= Math.ceil(searchResults.length / imagesPerPage); i++) {
+  for (let i = 1; i <= Math.ceil((searchResults?.length||0) / imagesPerPage); i++) {
     pageNumbers.push(i);
   }
 
@@ -72,42 +71,48 @@ export const Selldata = () => {
         initialSearchValue={initialSearchValue}
         Clear={handleClearSearch}
       />
-      <div className="flex flex-wrap justify-center gap-8 my-20 md:my-20 md:mx-0 lg:mx-24">
-        {searchResults
-          .slice(indexOfFirstImage, indexOfLastImage)
-          .map((product, index) => (
-            <NewCard key={index} product={product} />
-          ))}
-      </div>
-      <div className="flex justify-center">
-        <div className="text-2xl mb-12 flex">
-          <button
-            onClick={() => handleClick(Math.max(currentPage - 1, 1))}
-            disabled={currentPage === 1}
-          >
-            ◀️
-          </button>
-          <div>
-            {pageNumbers.map((number) => (
-              <button
-                key={number}
-                onClick={() => handleClick(number)}
-                className={currentPage === number ? "font-bold" : ""}
-              >
-                {number}
-              </button>
-            ))}
+      {Array.isArray(searchResults) && searchResults.length > 0 ? (
+        <>
+          <div className="flex flex-wrap justify-center gap-8 my-20 md:my-20 md:mx-0 lg:mx-24">
+            {searchResults
+              .slice(indexOfFirstImage, indexOfLastImage)
+              .map((product, index) => (
+                <NewCard key={index} product={product} />
+              ))}
           </div>
-          <button
-            onClick={() =>
-              handleClick(Math.min(currentPage + 1, pageNumbers.length))
-            }
-            disabled={currentPage === pageNumbers.length}
-          >
-            ▶️
-          </button>
-        </div>
-      </div>
+          <div className="flex justify-center">
+            <div className="text-2xl mb-12 flex">
+              <button
+                onClick={() => handleClick(Math.max(currentPage - 1, 1))}
+                disabled={currentPage === 1}
+              >
+                ◀️
+              </button>
+              <div>
+                {pageNumbers.map((number) => (
+                  <button
+                    key={number}
+                    onClick={() => handleClick(number)}
+                    className={currentPage === number ? 'font-bold' : ''}
+                  >
+                    {number}
+                  </button>
+                ))}
+              </div>
+              <button
+                onClick={() =>
+                  handleClick(Math.min(currentPage + 1, pageNumbers.length))
+                }
+                disabled={currentPage === pageNumbers.length}
+              >
+                ▶️
+              </button>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="flex justify-center my-20 text-xl">No Results</div>
+      )}
     </>
   );
 };
