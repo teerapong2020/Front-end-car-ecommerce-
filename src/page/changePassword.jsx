eaeb6c4d8b110a11341738029850fc541533692d
Guitar <kittipong.sataya@gmail.com>
2024-07-14 00:39:13 +0700

    v

--- src/page/changePassword.jsx
+++ src/page/changePassword.jsx
@@ -7,40 +7,22 @@ import { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 import UserMenu from "../components/component_userprofile/usermenu";
 import EditUser from "../components/component_userprofile/edituser";
+import Chang from "./uploadprofile";
+import EditPassWord from "../components/component_userprofile/editpassword";
 
-function UserProfile() {
-
-
+function ChangePassPage() {
   return (
     <div className="flex flex-col items-center text-base">
-   
       <div className="w-[1128px] flex gap-x-[24px] mt-16">
         <div className="flex flex-col w-[264px]">
           <UserMenu />
         </div>
         <div className="flex flex-col w-[840px]">
-          <EditUser/>
+          <EditPassWord />
         </div>
-        
       </div>
     </div>
   );
 }
 
-export default UserProfile;
-
-
-//   let { state } = useLocation();
-
-//   const [transaction, setTransaction] = useState(state);
-//   useEffect(() => {
-//     const token = localStorage.getItem("token");
-//     const id = jwtDecode(token).id;
-//     setTransaction((prevTransaction) => ({
-//       ...prevTransaction,
-//       address: "",
-//       etc: "",
-//       Purchase_User: id,
-//     }));
-//   }, []);
-  // console.log("transaction:", transaction);
+export default ChangePassPage;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
-export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL = "https://back-end-car-ecommerce-5-ptu0.onrender.com/api";
+
+// "http://localhost:5000/api"
